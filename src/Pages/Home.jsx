4dd27574a9dadf480875069fd57d4587dca10042
hanlee-311 <013hannah.a.lee@gmail.com>
2025-10-26 12:39:52 -0400

    testing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
             <div className="wrapper">
                 <div className="active-menu-wrapper">
                     <p>Active Menu List (checked boxes will appear on website)</p>
-                    <p>Add Section to Active Menu List</p>
+                    <p>Add Section to Active Menu List Test</p>
                 </div>
             </div>
         </>
